5d9e6848ed49e41f5dc248fc35957a7129cc3a10
leo.lin <leo.lin@advantech.com.tw>
2025-11-30 11:28:12 +0800

    feat: 完成 header 優化

--- frontend/src/app/layout/header/header.ts
+++ frontend/src/app/layout/header/header.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { Menu } from 'primeng/menu';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, Menu, Button],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {
+export class Header implements OnInit {
+  items: MenuItem[] | undefined;
 
+  ngOnInit() {
+    this.items = [
+      {
+        label: '總覽看板',
+        icon: 'pi pi-chart-bar',
+        routerLink: '/dashboard'
+      },
+      {
+        label: '交易明細',
+        icon: 'pi pi-list',
+        routerLink: '/transactions'
+      },
+      {
+        label: 'AI 趨勢分析',
+        icon: 'pi pi-chart-line',
+        routerLink: '/analysis'
+      },
+      {
+        label: 'AI 智慧搜尋',
+        icon: 'pi pi-search',
+        routerLink: '/smart-search'
+      }
+    ];
+  }
 }
